74aeb4fbe6d0a13bf98d1e9adeb19bab6eca6820
Alfie Rayner <alfie.rayner30@gmail.com>
2023-02-25 20:07:39 +0000

    colour

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -1,7 +1,7 @@
 const Hero = (props:any) => {
     return (
         <div>
-            <button type="button">Enter</button>
+            <button type="button" className="flex top-auto divide-red-600">Enter</button>
             <br/>
             hahar alex
         </div>
