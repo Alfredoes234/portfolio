c19de45323b7d493661d4d1cf492bf254daf97ce
Alfie Rayner <alfie.rayner30@gmail.com>
2023-08-07 17:12:57 +0100

    lol

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
+    <>
       <div>
         Stuff
       </div>
-    </main>
+      <div>
+        <iframe className="text-lg w-330px h-191px" src="https://api.eu.badgr.io/public/assertions/e_nGgcaXTxWn-n3WlzF4XA?embedVersion=1&amp;embedWidth=330&amp;embedHeight=191&amp;identity__email=arayner%40student.strode-college.ac.uk" title="Badge: Working in a Digital World: Professional Skills"></iframe>
+      </div>
+    </>
   );
 }
