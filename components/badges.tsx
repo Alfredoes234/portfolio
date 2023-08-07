71e6cec8dfa9536f20c4034346869a798e92beba
Alfie Rayner <alfie.rayner30@gmail.com>
2023-08-07 17:32:37 +0100

    ahh?

--- components/badges.tsx
+++ components/badges.tsx
@@ -2,8 +2,11 @@
 export default function Badges() {
     return(
         <>
-            <iframe className="text-lg w-330px h-300px border-none" src="https://api.eu.badgr.io/public/assertions/e_nGgcaXTxWn-n3WlzF4XA?embedVersion=1&amp;embedWidth=330&amp;embedHeight=191&amp;identity__email=arayner%40student.strode-college.ac.uk" title="Badge: Working in a Digital World: Professional Skills"></iframe>
-            <iframe className="text-lg w-330px h-300px border-none" src="https://api.eu.badgr.io/public/assertions/UXNjT1esRRehjBUvn6EtHA?embedVersion=1&amp;embedWidth=330&amp;embedHeight=191&amp;identity__email=techfortea%40gmail.com" title="Badge: The Ethical IT Professional"></iframe>
+            <div className="text-lg w-330px h-300px border-none">
+                <iframe src="https://api.eu.badgr.io/public/assertions/e_nGgcaXTxWn-n3WlzF4XA?embedVersion=1&amp;embedWidth=330&amp;embedHeight=191&amp;identity__email=arayner%40student.strode-college.ac.uk" title="Badge: Working in a Digital World: Professional Skills"></iframe>
+                <iframe src="https://api.eu.badgr.io/public/assertions/UXNjT1esRRehjBUvn6EtHA?embedVersion=1&amp;embedWidth=330&amp;embedHeight=191&amp;identity__email=techfortea%40gmail.com" title="Badge: The Ethical IT Professional"></iframe>
+            </div>
+            
         </>
     );
 }
