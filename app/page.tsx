2840410a9714f94d2eb80ff0b9d0648ccf553b8c
Alfie Rayner <alfie.rayner30@gmail.com>
2023-08-07 17:19:52 +0100

    lol

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Badges from "@/components/badges";
 import Image from "next/image";
 
 export default function Home() {
@@ -7,7 +8,7 @@ export default function Home() {
         Stuff
       </div>
       <div>
-        <iframe className="text-lg w-330px h-191px" src="https://api.eu.badgr.io/public/assertions/e_nGgcaXTxWn-n3WlzF4XA?embedVersion=1&amp;embedWidth=330&amp;embedHeight=191&amp;identity__email=arayner%40student.strode-college.ac.uk" title="Badge: Working in a Digital World: Professional Skills"></iframe>
+        <Badges/>
       </div>
     </>
   );
